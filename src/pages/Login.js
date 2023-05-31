82baf282f92dd4fabf807c0192fba4439fff4656
zarasah <zarinasah1989@gmail.com>
2023-05-31 23:21:33 +0400

    add validation for register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,68 +1,119 @@
 import './Login.css';
-import { useState } from "react";
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 import { useNavigate, Link } from "react-router-dom";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const [isError, setIsError] = useState(false);
     const navigate = useNavigate();
+    const [isError, setIsError] = useState(false);
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        
-        if (!email || !password) {
-            setMessage('All fields are required');
-            setIsError(true);
-            return;
-        }
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
+        mode: 'onChange'
+    });
 
-        const data = {
-            email,
-            password
-        }
-
-        fetch('http://localhost:4000/login', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
-        })
-        .then(res => {
-            if (res.status === 401) {
-                setMessage('Incorrect email or password');
-                setIsError(true);
-            }
-            return res.json();
-        })
-        .then(res => {
-            const user = JSON.stringify(res);
-            localStorage.setItem('user', user);
-            
-            if (res.role === 1) {
-                navigate('/admin');
-            } else if (res.role === 0){
-                navigate('/user');
-            } else {
-                navigate('/login');
-            }
-        })
-        .catch(error => {console.log(error)})
+    function onSubmit(data) {
+        console.log(data); // here will be dispatcher and we will send data to the server
+        reset();
     }
-
+    
     return (
         <div className = "login-page">
             <div className = "form">
-                <form className = "login-form" onSubmit={handleSubmit}>
-                    <input type = "email" placeholder = "Email Address*" onChange={(event) => setEmail(event.target.value)}  required />
-                    <input type = "password" placeholder = "Password*" onChange={(event) => setPassword(event.target.value)}  required />
+                <form className = "login-form" onSubmit={handleSubmit(onSubmit)}>
+                    <input 
+                        type = "email" 
+                        placeholder = "Email Address*"
+                        {
+                            ...register('email', {
+                                required: 'Email is required',
+                                pattern: {
+                                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                    message: 'Please enter a valid email address.'
+                                },
+                            })
+                        }
+                    />
+                    <div className='error'>{errors?.email && <span>{errors?.email?.message || 'Errors'}</span>}</div>
+                    <input 
+                        type = "password" 
+                        placeholder = "Password*"
+                        {
+                            ...register('password', {
+                                required: 'Password is required',
+                            })
+                        }
+                    />
+                    <div className='error'>{errors?.password && <span>{errors?.password?.message || 'Errors'}</span>}</div>
                     <button>Sign In</button>
                     <p className = "message" >Not registered? <Link to = "/register">Create an account</Link></p>
-                    <p className = {isError ? "error-active" : "error"}>{message}</p>
+                    <p className = {isError ? "error-active" : "error"}></p>
                 </form>
             </div>
         </div>
     )
 }
+
+// export default function Login() {
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+//     const [message, setMessage] = useState('');
+//     const [isError, setIsError] = useState(false);
+//     const navigate = useNavigate();
+
+//     function handleSubmit(event) {
+//         event.preventDefault();
+        
+//         if (!email || !password) {
+//             setMessage('All fields are required');
+//             setIsError(true);
+//             return;
+//         }
+
+//         const data = {
+//             email,
+//             password
+//         }
+
+//         fetch('http://localhost:4000/login', {
+//             method: 'POST',
+//             body: JSON.stringify(data),
+//             headers: {
+//                 'Content-type': 'application/json; charset=UTF-8',
+//             }
+//         })
+//         .then(res => {
+//             if (res.status === 401) {
+//                 setMessage('Incorrect email or password');
+//                 setIsError(true);
+//             }
+//             return res.json();
+//         })
+//         .then(res => {
+//             const user = JSON.stringify(res);
+//             localStorage.setItem('user', user);
+            
+//             if (res.role === 1) {
+//                 navigate('/admin');
+//             } else if (res.role === 0){
+//                 navigate('/user');
+//             } else {
+//                 navigate('/login');
+//             }
+//         })
+//         .catch(error => {console.log(error)})
+//     }
+
+//     return (
+//         <div className = "login-page">
+//             <div className = "form">
+//                 <form className = "login-form" onSubmit={handleSubmit}>
+//                     <input type = "email" placeholder = "Email Address*" onChange={(event) => setEmail(event.target.value)}  required />
+//                     <input type = "password" placeholder = "Password*" onChange={(event) => setPassword(event.target.value)}  required />
+//                     <button>Sign In</button>
+//                     <p className = "message" >Not registered? <Link to = "/register">Create an account</Link></p>
+//                     <p className = {isError ? "error-active" : "error"}>{message}</p>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,71 +1,158 @@
 import './Login.css';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 export default function Register() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const [showModal, setShowModal] = useState(false);
-    const [isError, setIsError] = useState(false);
+    const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm({
+        mode: 'onChange'
+    });
 
-    function register(event) {
-        event.preventDefault();
-
-        if (!(name && email && password)) {
-            setMessage('All fields are required');
-            setIsError(true);
-            return;
-        }
-
-        const data = {
-            name,
-            email,
-            password
-        }
-
-        fetch('http://localhost:4000/register', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
-        })
-        .then(res => {
-            if (res.status === 409) {
-                setMessage('Email already exists');
-                setIsError(true);
-                return
-            } else {
-                setMessage('Congratulations! Your registration was successful.');
-                setShowModal(true);
-            }
-        })
-        .catch(error => {console.log(error)})
+    function onSubmit(data) {
+        console.log(data); // here will be dispatcher and we will send data to the server
+        reset();
     }
-    
+        
     return (
         <div className="login-page">
             <div className="form">
-                {
-                    !showModal ? (
-                        <form className="register-form" onSubmit = {register}>
-                            <input type="text" placeholder="Name" onChange={(event) => setName(event.target.value)}/>
-                            <input type="text" placeholder="Email Address" onChange={(event) => setEmail(event.target.value)}/>
-                            <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
-                            <button>create</button>
-                            <p className="message">Already registered? <Link to = "/login">Sign In</Link></p>
-                            <p className = {isError ? "error-active" : "error"}>{message}</p>
-                        </form>
-                    ) : (
-                        <div>
-                            <p className='registered'>{message}</p>
-                            <Link to = "/login">Go to login page</Link>
-                        </div>
-                    )
-                }
+                <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
+                    <input 
+                        type="text"
+                        placeholder="First Name"
+                        {
+                            ...register('firstname', {
+                                required: 'Fisrt name is required',
+                                minLength: {
+                                    value: 3,
+                                    message: 'First name must be at least 3 chars'
+                                },
+                            })
+                        }
+                    />
+                    <div className='error'>{errors?.firstname && <span>{errors?.firstname?.message || 'Errors'}</span>}</div>
+                    <input 
+                        type="text" 
+                        placeholder="Last Name"
+                        {
+                            ...register('lastname')
+                        }
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Email Address"
+                        {
+                            ...register('email', {
+                                required: 'Email is required',
+                                pattern: {
+                                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                    message: 'Please enter a valid email address.'
+                                },
+                            })
+                        }
+                    />
+                    <div className='error'>{errors?.email && <span>{errors?.email?.message || 'Errors'}</span>}</div>
+                    <input 
+                        type="password" 
+                        placeholder="Password"
+                        {
+                            ...register('password', {
+                                required: 'Password is required',
+                                minLength: {
+                                    value: 8,
+                                    message: 'Password must be at least 8 chars' 
+                                },
+                                pattern: {
+                                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]*$/,
+                                    message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'
+                                }
+                            })
+                        }
+                    />
+                    <div className='error'>{errors?.password && <span>{errors?.password?.message || 'Errors'}</span>}</div>
+                    <input className = 'button' type = 'submit' disabled = {!isValid}/>
+                    {/* <button disabled = {!isValid}>create</button> */}
+                    <p className="message">Already registered? <Link to = "/login">Sign In</Link></p>
+                </form>
+                <div>
+                    <p className="message"><Link to = "/login">Go to login page</Link></p>
+                </div>
             </div>
         </div>
     )
 }
+
+// import './Login.css';
+// import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+// export default function Register() {
+//     const [firstName, setFirstName] = useState('');
+//     const [lastName, setLastName] = useState('');
+//     const [name, setName] = useState('');
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+//     const [message, setMessage] = useState('');
+//     const [showModal, setShowModal] = useState(false);
+//     const [isError, setIsError] = useState(false);
+
+//     function register(event) {
+//         event.preventDefault();
+
+//         if (!(name && email && password)) {
+//             setMessage('All fields are required');
+//             setIsError(true);
+//             return;
+//         }
+
+//         const data = {
+//             name,
+//             email,
+//             password
+//         }
+
+//         fetch('http://localhost:4000/register', {
+//             method: 'POST',
+//             body: JSON.stringify(data),
+//             headers: {
+//                 'Content-type': 'application/json; charset=UTF-8',
+//             }
+//         })
+//         .then(res => {
+//             if (res.status === 409) {
+//                 setMessage('Email already exists');
+//                 setIsError(true);
+//                 return
+//             } else {
+//                 setMessage('Congratulations! Your registration was successful.');
+//                 setShowModal(true);
+//             }
+//         })
+//         .catch(error => {console.log(error)})
+//     }
+    
+//     return (
+//         <div className="login-page">
+//             <div className="form">
+//                 {
+//                     !showModal ? (
+//                         <form className="register-form" onSubmit = {register}>
+//                             <input type="text" placeholder="First Name" onChange={(event) => setFirstName(event.target.value)}/>
+//                             <input type="text" placeholder="Last Name" onChange={(event) => setLastName(event.target.value)}/>
+//                             <input type="text" placeholder="Name" onChange={(event) => setName(event.target.value)}/>
+//                             <input type="text" placeholder="Email Address" onChange={(event) => setEmail(event.target.value)}/>
+//                             <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
+//                             <button>create</button>
+//                             <p className="message">Already registered? <Link to = "/login">Sign In</Link></p>
+//                             <p className = {isError ? "error-active" : "error"}>{message}</p>
+//                         </form>
+//                     ) : (
+//                         <div>
+//                             <p className='registered'>{message}</p>
+//                             <Link to = "/login">Go to login page</Link>
+//                         </div>
+//                     )
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
